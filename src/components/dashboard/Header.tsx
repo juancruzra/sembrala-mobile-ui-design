336a995162dab92c676d694d77064ec77bd619d3
juancruzra <juancruz@sembrala.com>
2025-06-09 20:07:49 -0300

    Update Header.tsx

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Bell, User } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
